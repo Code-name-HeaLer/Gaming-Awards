664ef5892c3b97838929beee9ca2dd3a4e8741a5
CodeNameHealer <swagatprasad3344@gmail.com>
2025-01-23 21:04:35 +0530

    Project initialised

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-3xl font-bold py-5 mt-5 flex justify-center items-center'>Welcome</h1>
+    </main>
+
+
+  )
+}
+
+export default App
